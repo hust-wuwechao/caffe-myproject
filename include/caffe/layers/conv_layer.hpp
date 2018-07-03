6ee93352068c2e8d3ad48d2c2a85859f0db7b328
hust-wuwenchao <2591099581@qq.com>
2018-07-03 20:53:43 +0800

    aaa

--- include/caffe/layers/conv_layer.hpp
+++ include/caffe/layers/conv_layer.hpp
@@ -67,7 +67,11 @@ class ConvolutionLayer : public BaseConvolutionLayer<Dtype> {
   virtual inline const char* type() const { return "Convolution"; }
 
  protected:
- 
+  // 这四种函数是就卷积必须学会的的。
+  //
+  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+    const vector<Blob<Dtype>*>& top, cudnnHandle_t* handle , cudaStream_t*  stream) ;
+
   virtual void Forward_cpu(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
   virtual void Forward_gpu(const vector<Blob<Dtype>*>& bottom,
