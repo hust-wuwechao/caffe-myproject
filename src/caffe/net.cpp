3b3edb18cf1a5f44611b58f3288897db01d55ce8
hust-wuwenchao <2591099581@qq.com>
2018-07-04 22:23:02 +0800

    aaa

--- src/caffe/net.cpp
+++ src/caffe/net.cpp
@@ -150,11 +150,11 @@ void Net<Dtype>::Init(const NetParameter& in_param)
 
 
     
-     if(layers_[layer_id]->type()=="Convolution")
+    /*  if(layers_[layer_id]->type()=="Convolution")
     {
          LOG_IF(INFO, Caffe::root_solver()) << " CuDNNConvolutionLayer ";
          layers_[layer_id]->SetUp(bottom_vecs_[layer_id], top_vecs_[layer_id],handle_,stream_);
-    } 
+    }  */
 
 
     //else 
