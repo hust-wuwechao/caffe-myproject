ee484e7a1c5680fb61b3e974bc497b90cb408a93
hust-wuwenchao <2591099581@qq.com>
2018-07-04 21:05:34 +0800

    aaa

--- include/caffe/layers/cudnn_conv_layer.hpp
+++ include/caffe/layers/cudnn_conv_layer.hpp
@@ -35,8 +35,8 @@ class CuDNNConvolutionLayer : public ConvolutionLayer<Dtype> {
   virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top);
   
-  
-  virtual void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
+
+   void LayerSetUp(const vector<Blob<Dtype>*>& bottom,
     const vector<Blob<Dtype>*>& top, cudnnHandle_t* handle , cudaStream_t*  stream) ;
   
 
