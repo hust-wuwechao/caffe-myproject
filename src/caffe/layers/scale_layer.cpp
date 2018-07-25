5af8d5779a7ef59c756f3472903133a9301e1f84
hust-wuwenchao <2591099581@qq.com>
2018-07-25 16:53:23 +0800

    2018-07-25_16:53:23+提交进行的提交

--- src/caffe/layers/scale_layer.cpp
+++ src/caffe/layers/scale_layer.cpp
@@ -7,7 +7,8 @@
 #include "caffe/util/math_functions.hpp"
 
 namespace caffe {
-
+#define CUDNN_STREAMS_PER_GROUP 1
+#define GROUP 1
 template <typename Dtype>
 void ScaleLayer<Dtype>::LayerSetUp(const vector<Blob<Dtype>*>& bottom,
       const vector<Blob<Dtype>*>& top) {
