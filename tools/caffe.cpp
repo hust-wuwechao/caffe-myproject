17aa2f99b20c010f5c75aa87c743041823aeec4b
hust-wuwenchao <2591099581@qq.com>
2018-12-28 10:27:22 +0800

    2018-12-28_10:27:22+提交进行的提交

--- tools/caffe.cpp
+++ tools/caffe.cpp
@@ -399,7 +399,7 @@ int time() {
                           bottom_vecs[i]);
       //backward_time_per_layer[i] += timer.MicroSeconds();
     }
-    cudaDeviceSynchronize();
+    //cudaDeviceSynchronize();
     //backward_time += backward_timer.MicroSeconds();
      LOG(INFO) << "Iteration: " << j + 1 << " forward-backward time: "
       << iter_timer.MilliSeconds() << " ms."; 
